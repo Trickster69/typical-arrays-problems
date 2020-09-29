d313505706bb2244531a1b0050814ec86e1fcddb
Sergei Demchuk <vinci.ru@mail.ru>
2020-09-29 14:58:30 +0300

    feat: decide all task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if ((Array.isArray(array)) && (array.length > 0)) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((Array.isArray(array)) && (array.length > 0)) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((Array.isArray(array)) && (array.length > 0)) {
+        let sum = 0;
+        for (let key of array) {
+            sum += key;
+        }
+        return sum / array.length;
+    } else {
+        return 0;
+    }
+
 }
